d2c85bc400f75fdf7aa060877d9eacda2458d394
Haoming <haoming_xu@pfgamer.com>
2024-04-03 14:53:22 +0800

    format update

--- src/main/java/com/citizons/dev/whitelist/PluginMain.java
+++ src/main/java/com/citizons/dev/whitelist/PluginMain.java
@@ -6,16 +6,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 public final class PluginMain extends JavaPlugin {
 
     static PluginMain instance = null;
     static FileConfiguration config = null;
+    static Logger log = null;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getLogger().info("Zons Whitelist is initializing.");
+        log = getLogger();
+        log.info("Zons Whitelist is initializing.");
         saveDefaultConfig();
         Bukkit.getPluginManager().registerEvents(new ZEventHandler(), this);
         Objects.requireNonNull(this.getCommand("zonsw")).setExecutor(new ZCommandHandler());
@@ -37,9 +40,14 @@ public final class PluginMain extends JavaPlugin {
         var blacklistedPlayers = config.getList("blacklisted-players");
         if (whitelistedPlayers != null) {
             for (Object whitelistedPlayer : whitelistedPlayers) {
-                ZDataHandler.updateWhitelist(
-                        UUID.fromString((String) whitelistedPlayer)
-                );
+                try {
+                    ZDataHandler.updateWhitelist(
+                            UUID.fromString((String) whitelistedPlayer)
+                    );
+                } catch (Exception error) {
+                    log.info(String.format(
+                            "load config failed due to %s", error.toString()));
+                }
             }
         }
         if (blacklistedPlayers != null) {
--- src/main/java/com/citizons/dev/whitelist/ZCommandHandler.java
+++ src/main/java/com/citizons/dev/whitelist/ZCommandHandler.java
@@ -6,23 +6,25 @@ import org.bukkit.command.CommandSender;
 
 import java.util.Objects;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 public class ZCommandHandler implements CommandExecutor {
-
+    static Logger log = PluginMain.log;
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("zonsw")) {
             if (args.length == 2 && Objects.equals(args[0], "add")) {
                 ZDataHandler.updateWhitelist(UUID.fromString(args[1].toLowerCase()));
                 ZDataHandler.saveWhitelist();
-                PluginMain.instance.getLogger().info(
+                log.info(
                         String.format("Added UUID to whitelist: %s", args[1]));
                 sender.sendMessage("Successfully added UUID to whitelist");
                 return true;
             }
             if (args.length == 1 && Objects.equals(args[0], "reload")) {
                 PluginMain.instance.loadConfigs();
-                PluginMain.instance.getLogger().info("Reload complete");
+                log.info("Reload complete");
+                sender.sendMessage("Reload config complete");
                 return true;
             }
         }
